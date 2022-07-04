88eba620740655abb1bb80b3432dda50c8663b4d
Roberto Carlos <rj-work@hotmail.com>
2022-07-04 09:29:26 -0300

    a

---
+++ fsphp/02-iniciando-do-zero-com-php/02-04-variaves-e-tipos-de-dados/index.php
@@ -0,0 +1,91 @@
+<?php
+require __DIR__ . '/../../fullstackphp/fsphp.php';
+fullStackPHPClassName("02.04 - Variáveis e tipos de dados");
+
+/**
+ * [tipos de dados] https://php.net/manual/pt_BR/language.types.php
+ * [ variáveis ] https://php.net/manual/pt_BR/language.variables.php
+ */
+fullStackPHPClassSession("variáveis", __LINE__);
+
+$userFisrtName="Roberto";
+$userLastName="Carlos";
+$userAge="25";
+$userEmail="roberto@hotmail.com";
+echo "<h3>{$userFisrtName} {$userLastName}</h3>";
+echo "<p>{$userFisrtName} {$userLastName} <span class ='tag'>tem {$userAge} anos</spanclass></p>";
+echo "<p>O Email de {$userFisrtName} é <span class='tag'>{$userEmail}</span></p>";
+
+$calcA=10;
+$calcB=20;
+
+$calcB = &$calcA;
+$calcB = "50";
+
+var_dump([
+    "a"=>$calcA,
+    "b"=>$calcB,
+]);
+
+/**
+ * [ tipo boleano ] true | false
+ */
+fullStackPHPClassSession("tipo boleano", __LINE__);
+
+$true = true;
+$false = false;
+
+var_dump($true,$false);
+
+$bestAge = ($userAge > 50);
+var_dump($bestAge);
+
+$a = 0;
+$b = 0.0;
+$c = "";
+$d = [];
+$e = null;
+
+var_dump($a,$b,$c,$d,$e);
+
+if ($a || $b || $c || $d || $e){
+    var_dump(true);
+}else{
+    var_dump(false);
+}
+
+
+/**
+ * [ tipo callback ] call | closure
+ */
+fullStackPHPClassSession("tipo callback", __LINE__);
+
+$code = "<article><h1>Um Call User Function!</h1></article>";
+$codeClear = call_user_func("strip_tags",$code);
+var_dump($code,$codeClear);
+$codeMore = function ($code){
+    var_dump($code);
+};
+$codeMore("#BoraProgramar!");
+
+/**
+ * [ outros tipos ] string | array | objeto | numérico | null
+ */
+fullStackPHPClassSession("outros tipos", __LINE__);
+
+$string = "Hello World!";
+$array = [$string];
+$object = new stdClass();
+$object->hello = $string;
+$null = null;
+$int = 12132;
+$float = 1.23213;
+
+var_dump([
+    $string,
+    $array,
+    $object,
+    $null,
+    $int,
+    $float,
+]);
