73df8d2465a5157fa50949c648ce0ac4c5f3446e
Roberto <rj-evil@hotmail.com>
2022-09-15 16:05:18 -0300

    Att

--- fsphp/04-php-orientado-a-objetos/04-03-qualificacao-e-encapsulamento/index.php
+++ fsphp/04-php-orientado-a-objetos/04-03-qualificacao-e-encapsulamento/index.php
@@ -7,12 +7,35 @@ fullStackPHPClassName("04.03 - Qualificação e encapsulamento");
  */
 fullStackPHPClassSession("namespaces", __LINE__);
 
+require __DIR__."/classes/App/Template.php";
+require __DIR__."/classes/Web/Template.php";
+
+$appTemplate = new App\Template();
+$webTemplate = new Web\Template();
+
+var_dump(
+    $appTemplate,
+    $webTemplate
+);
+
+
+use App\Template;
+use Web\Template AS WebTemplate;
+
+$appUseTemplate = new Template();
+$webUseTemplate = new WebTemplate();
+
+var_dump(
+    $appUseTemplate,
+    $webUseTemplate,
+);
 
 /*
  * [ visibilidade ] http://php.net/manual/pt_BR/language.oop5.visibility.php
  */
 fullStackPHPClassSession("visibilidade", __LINE__);
 
+    
 
 /*
  * [ manipulação ] Classes com estruturas que abstraem a rotina de manipulação de objetos
