e3736465cbcaffb9667b856aa3df5c60303894df
Roberto <rj-evil@hotmail.com>
2022-09-19 19:57:27 -0300

    criacao

---
+++ Full/PHP/funcoes.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP</title>
+</head>
+<body>
+
+    <?php
+
+        //void (sem retorno)
+        function exibirBoasVindas(){
+            echo "Bem vindo ao PHP ! <br>";
+        }
+        
+        exibirBoasVindas();
+
+        //return (com retorno)
+        function calcularAreaTerreno($largura,$comprimento){
+
+        }
+    ?>
+    
+</body>
+</html>
